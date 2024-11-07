852b1bb6292e92f949f03a640481e52df257d752
Seblor <github@seblor.fr>
2024-11-07 20:52:31 +0100

    0.2.0

--- src/lib/UpdateChecker.ts
+++ src/lib/UpdateChecker.ts
@@ -1,13 +1,13 @@
 const { app } = require("@electron/remote");
 
-export async function checkForUpdate(): Promise<{newUpdateAvailable: boolean, latestUpdateVersion: string}> {
+export async function checkForUpdate (): Promise<{ newUpdateAvailable: boolean, latestUpdateVersion: string }> {
   const apiUrl = 'https://api.github.com/repos/Seblor/presscorp-stream-controller/releases/latest';
 
   const response = await fetch(apiUrl);
   const data = await response.json();
   const latestVersion = data.tag_name;
   const currentVersion = app.getVersion();
-  if (latestVersion === currentVersion) {
+  if (latestVersion === currentVersion || latestVersion === 'v' + currentVersion) {
     return { newUpdateAvailable: false, latestUpdateVersion: latestVersion };
   }
   return { newUpdateAvailable: true, latestUpdateVersion: latestVersion };
